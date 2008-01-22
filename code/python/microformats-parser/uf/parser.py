1d1d9f0c59eced709e13270aee70443ad37e1276
aconbere <aconbere@3aa6e902-003e-0410-b24d-b35526dee1fc>
2008-01-22 18:51:28 +0000

    moving transform into the filters to allow pre and post processing, adding documentation.

--- code/python/microformats-parser/uf/parser.py
+++ code/python/microformats-parser/uf/parser.py
@@ -10,6 +10,10 @@ def get_abs_path(file_name):
 
 class Parser(object):
     def _open_stream(self, source):
+        """
+        _open_stream takes either a string or a file like object, and returns the body
+        of that input. (a string)
+        """
         if hasattr(source, 'read'):
             stream = source.read()
         else:
@@ -22,10 +26,20 @@ class Parser(object):
         return stream
 
 class XHTMLParser(Parser):
+    xsl_file = ""
     filters = []
+    
+    def __init__(self):
+        self.add_filter(self._transform())
+        self.add_filter(self._serialize())
 
     def parse(self, stream):
-        return self.run_filters(self._transform(self._open_stream(stream)))
+        """
+        Parse takes a stream, in this case either a string or a file like object
+        (anything that impliments a read function), this could be a url opened with
+        urllib, or an opened file.
+        """
+        return self.run_filters(self._open_stream(stream))
 
     def add_filter(self, filter):
         self.filters.append(filter)
@@ -38,57 +52,87 @@ class XHTMLParser(Parser):
             return input
         return _run_filters(self.filters, input)
 
-    def _transform(self, stream):
-        input = etree.HTML(self._open_stream(stream))
-        xsl = etree.parse(self.xsl_file)
-        transform = etree.XSLT(xsl)
-        output = transform(input)
-        return output
+    def _serialize(self):
+        """
+        The serialize filter takes a stream and returns an etree representation of that xml
+        """
+        def xml_filter(stream):
+            return etree.HTML(self._open_stream(stream))
+        return xml_filter
 
-    def _open_stream(self, source):
-        if hasattr(source, 'read'):
-            stream = source.read()
-        else:
-            if isinstance(source, unicode):
-                stream = source.encode('utf-8')
-            elif isinstance(source, str):
-                stream = source
-            else:
-                stream = str(source)
-        return stream
+    def _transform(self):
+        """
+        Transfroms the xml representation of a microformat into a more suitable format via xsl
+        In most cases this will be converting it into the format that the microformat
+        derives from.
+        
+        Example.
+            hCal is an implimentation of vCal, the transform here turns an xmlized
+            hCal into a properly formed vCal
+        """
+        def xsl_filter(xml):
+            xsl = etree.parse(self.xsl_file)
+            transform = etree.XSLT(xsl)
+            return transform(xml)
+        return xsl_filter
+
+    def _cut_xml(self):
+        """
+        This is a yet to be implimented filter that should take an xml representation (etree)
+        of the html data, and then cut it into a list of the fragments it will be parsing.
+        
+        Example.
+            A single html document might include many hCards, this filter will take the
+            xmlized form of that document and return a list of those hCards as xml fragments
+        """
+        def xpath_filter(stream):
+            pass
 
 class vObjParser(XHTMLParser):
+    """
+    The vObjParser is used to convert XHTML to vObjects
+    """
     def __init__(self):
-        super(Parser, self).__init__()
         self.add_filter(filters.to_vobject)
         self.add_filter(filters.to_str)
+        super(vObjParser, self).__init__()
 
 class hCard(vObjParser):
+    """
+    Parses hCards in vCards in vObject
+    """
     xsl_file = get_abs_path("./xsl/xhtml2vcard.xsl")
 
 class hCal(vObjParser):
+    """
+    Parses hCals into vCals in vObject
+    """
     xsl_file = get_abs_path("./xsl/xhtml2vcal.xsl")
 
 class hAtom(XHTMLParser):
+    """
+    Parses hAtoms into Atoms in feedparser
+    """
     xsl_file = get_abs_path("./xsl/hAtom2Atom.xsl")
 
     def __init__(self):
-        super(Parser, self).__init__()
         self.add_filter(filters.atom2python)
         self.add_filter(filters.to_str)
+        super(hAtom, self).__init__()
 
 class LinkParser(Parser):
     """
-    URLParsers uses the rel attribute to assign meta data to the urls.
+    LinkParsers uses the rel attribute to assign meta data to the urls.
     This is the parent class of XFNParser and RelTagParser
+    
+    Right now this only handles 'a' tags, to obey the spec it should
+    be able to grab either 'a' or 'link' tags.
     """
     properties = set()
 
     # it would be nice to be able to send in a Beautiful SoupStrainer
     # so we don't parse the whole page everytime
     def parse(self, stream):
-        # self.path = path
-        # self.source = fetch(path)
         return self.get_links(self._transform(self._open_stream(stream)))
     
     def _transform(self, stream):
